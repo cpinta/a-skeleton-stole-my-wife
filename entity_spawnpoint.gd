1ed2b32f4119a5bf10d0d7b744a4c38a36374d86
cpinta <pinta.christian@outlook.com>
2024-10-20 16:00:23 -0500

    adjusted tilempa

--- entity_spawnpoint.gd
+++ entity_spawnpoint.gd
@@ -1,23 +1,34 @@
 extends Node2D
 class_name Entity_Spawnpoint
 
-var spawnPool: Array[Entity]
-var spawnChances: Array[float]
+@export var spawnPool: Array[Entity]
+@export var spawnWeights: Array[int]
+var spawnWeightTotal: int = 0
 
 var SPAWN_TIMER_MIN: float = 3
 var SPAWN_TIMER_MAX: float = 6
-var SPAWN_TIME_SCALES: bool = true
+var SPAWNS_SCALE_W_TIME: bool = true
 var spawnTimer: float = 0
 
 var level: Level
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	level = get_tree().get_nodes_in_group("level")[0]
+	for weight in spawnWeights:
+		spawnWeightTotal += weight
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
+
+func get_random_spawn_from_weights():
+	var chosen = randi_range(0, spawnWeightTotal)
+	var count = spawnWeights[0]
+	var i = 0
+	while count < chosen:
+		count += spawnWeights[i]
+		i += 1
 	pass
