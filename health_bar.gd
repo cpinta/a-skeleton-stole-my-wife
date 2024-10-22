4544f6effd7687dee009a0344bd847183d1e7cc9
cpinta <pinta.christian@outlook.com>
2024-10-21 21:54:01 -0500

    adjusted health bar and fixed some crashes

--- health_bar.gd
+++ health_bar.gd
@@ -3,40 +3,46 @@ class_name UI_HealthBar
 
 var BAR_CHANGE_AMOUNT: float = 10
 var target: Entity
-var actual_value: float = 0
+var target_value: float = 0
 var firstFrameAfterHit: bool = false
+
+var lbl: Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	target = get_tree().get_nodes_in_group("player")[0]
 	max_value = target.health
 	value = target.health
-	actual_value = value
+	target_value = value
+	
+	lbl = $num
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if target != null:
+		target_value = target.health
 		if max_value != target.STARTING_HEALTH:
 			var diff: float = sign(target.health - value) * BAR_CHANGE_AMOUNT * delta
 			max_value += diff
 		if value != target.health:
 			if not firstFrameAfterHit:
 				var diff: float = sign(target.health - value) * BAR_CHANGE_AMOUNT * delta
-				actual_value += diff
-				if sign(target.health - actual_value) != sign(diff):
-					actual_value = target.health
+				value += diff
+				if sign(target.health - value) != sign(diff):
+					value = target.health
 			else:
 				firstFrameAfterHit = false
 		else:
 			firstFrameAfterHit = true
 	else:
+		target_value = 0
 		if value > 0:
 			var diff: float = -1 * BAR_CHANGE_AMOUNT * delta
-			actual_value += diff
-	
-	value = actual_value
+			value += diff
+			
+	lbl.text = str(target_value)+"/"+str(max_value)
 	pass
 
 func setup(maxHealth: int):
